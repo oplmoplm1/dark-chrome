a7741b4a389b40b5cbdc542f7cd516584d856aec
Liyang <oplmoplm2@gmail.com>
2015-06-16 23:48:33 +0800

    add comments
    add comments

--- a.js
+++ a.js
@@ -1,6 +1,9 @@
 window.onload = function(){
+	//得到当前时间的小时
 	var h = new Date().getHours();
+	//判断小时是否是晚上
 	if (h>=22 || h<=5){
+    	// 对所有元素通过map进行遍历，对所有元素设定background和color两个属性。
     	[].map.call(document.querySelectorAll('*'),function(item){ item.style.backgroundColor='#222222';item.style.color='#00ff00 ';});
 	}
 }
--- popup.js
+++ popup.js
@@ -4,15 +4,22 @@
 
 
 function click(e) {
+	// 假设div 具有 databg 和datafont两个属性
+	//得到背景颜色的配置
 	var bg = this.getAttribute('databg');
+	//得到字体颜色的配置
 	var font = this.getAttribute('datafont');
+	//调用executeScript方法来往页面注入代码，代码同a js
   chrome.tabs.executeScript(null,
       {code:"[].map.call(document.querySelectorAll('*'),function(item){ item.style.backgroundColor='"+bg+"';item.style.color='"+font+"';});"});
+  //关闭窗口
   window.close();
 }
-
+// 当popup页面加载完成后执行
 document.addEventListener('DOMContentLoaded', function () {
+  //得到加载完成后的所有div
   var divs = document.querySelectorAll('div');
+  //对所有div加监听事件 触发click方法，假设div 具有 databg 和datafont两个属性
   for (var i = 0; i < divs.length; i++) {
     divs[i].addEventListener('click', click);
   }
